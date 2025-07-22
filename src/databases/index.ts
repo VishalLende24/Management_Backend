87a2aebb410b983e11f2c2122fd8e72ca94e479a
Vishallende24 <vishallende24@gmail.com>
2025-07-22 11:16:58 +0530

    [ADD]: Mongo url

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -1,7 +1,7 @@
 import { DB_HOST, DB_PORT, DB_DATABASE } from '@config';
 
 export const dbConnection = {
-  url: `mongodb://${'localhost'}:${'27017'}/${'ASSIGMENT'}`,
+  url: `mongodb+srv://vishallende24:vishal24@cluster0.rhy2f.mongodb.net/assignment?retryWrites=true&w=majority&appName=Cluster0`,
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
