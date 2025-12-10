483f6c69970315057d4141021bf23e83d9c9c325
vishallende24 <vishallende24@gmail.com>
2025-12-10 11:56:26 +0530

    Mongo uri is updated in env

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -1,7 +1,7 @@
-import { DB_HOST, DB_PORT, DB_DATABASE } from '@config';
+import { DB_HOST, DB_PORT, DB_DATABASE, MONGO_URI } from '@config';
 
 export const dbConnection = {
-  url: `mongodb+srv://vishallende24:vishal24@cluster0.rhy2f.mongodb.net/assignment?retryWrites=true&w=majority&appName=Cluster0`,
+  url: MONGO_URI,
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
